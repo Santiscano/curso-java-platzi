2230dbb6309b47ae3c5bce9df27216e8899e9a18
santiago sierra cano <santiscano@gmail.com>
2025-04-12 12:47:29 -0500

    fix: Update generated date in ProductMapperImpl

--- 7-spring-boot/platzi_market-teacher/bin/generated-sources/annotations/com/platzi/market/persistence/mapper/ProductMapperImpl.java
+++ 7-spring-boot/platzi_market-teacher/bin/generated-sources/annotations/com/platzi/market/persistence/mapper/ProductMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-09T19:22:01-0500",
+    date = "2025-04-12T08:24:41-0500",
     comments = "version: 1.6.2, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
